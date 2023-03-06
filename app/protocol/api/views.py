d118348eda85fe1167ad5d5583055e7d0383b74f
gustavo brito <gustavo.brito@open-co.br>
2023-03-05 21:46:06 -0300

    CORS.

--- app/protocol/api/views.py
+++ app/protocol/api/views.py
@@ -1,16 +1,27 @@
+from pyramid.response import Response
 from pyramid.view import view_config
 
 from infra.storage import filter_product_name_contains, filter_product_name_equals
+
+DEFAULT_RESPONSE_HEADERS = {'Access-Control-Allow-Origin': '*'}
 
 
 @view_config(route_name='search', renderer='json')
 def search_view(request):
     q = request.params.get('q', None)
-    return filter_product_name_contains(q)
+    return Response(
+        content_type='application/json',
+        headers=DEFAULT_RESPONSE_HEADERS,
+        json_body=filter_product_name_contains(q)
+    )
 
 
 @view_config(route_name='detail', renderer='json')
 def detail_view(request):
     q = request.params.get('q', None)
     not_sold = True if request.params.get('include_not_sold', 'false') == 'true' else False
-    return filter_product_name_equals(q, not_sold)
+    return Response(
+        content_type='application/json',
+        headers=DEFAULT_RESPONSE_HEADERS,
+        json_body=filter_product_name_equals(q, not_sold)
+    )
